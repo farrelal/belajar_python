eb539b079bb686e4ae8133b58d044b03192a36b3
farrelal <140674376+farrelal@users.noreply.github.com>
2024-01-25 09:00:24 +0700

    Add files via upload

---
+++ pertemuan 3/operator_indentitas.py
@@ -0,0 +1,10 @@
+x = 5
+y = 5
+z = 6
+
+# operator indentitas IS
+print('x is y :', x is y)
+print('x is z :', x is z)
+
+# operator indentitas IS NOT
+print('x is not c :', is not z)
---
+++ pertemuan 3/operator_penugasan.py
@@ -0,0 +1,30 @@
+# penjumlahan 
+a = 5
+a +=3
+print(a)
+
+# pengurangan
+b = 8
+b -= 3
+print(b)
+ 
+# perkalian
+c = 4
+c *= 2
+print(c)
+
+# pembagian
+d = 7
+d /= 9
+print(d)
+
+# perpangkatan
+e = 6
+e **= 8
+print(e)
+
+# moduls
+f = 1
+f %= 10
+print(f)
+
